b84145d88cf18f990c3e4712cc2c72fe994c9fe1
enghui <enghui@u.nus.edu>
2024-03-23 12:48:31 +0800

    change title

--- SystemCode/backend/app.py
+++ SystemCode/backend/app.py
@@ -58,7 +58,7 @@ async def construct_result(text, blocks=None):
         "content": text,
         "createAt": timestamp,
         "extra": {},
-        "id": "2",
+        "id": str(timestamp),
         "meta": {
             "avatar": "https://images.unsplash.com/photo-1589656966895-2f33e7653819?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "title": "cinebear",
