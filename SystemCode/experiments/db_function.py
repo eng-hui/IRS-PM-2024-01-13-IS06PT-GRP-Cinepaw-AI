d7f13972ba5072f56db73be4f809a144e8818e32
loischee <156448154+loischee@users.noreply.github.com>
2024-03-26 23:36:57 +0800

    Update db_function.py

--- SystemCode/experiments/db_function.py
+++ SystemCode/experiments/db_function.py
@@ -3,10 +3,6 @@ import sqlite3
 # Connect to the database
 conn = sqlite3.connect('movies.db')
 
-# Input variables for the SQL query
-#movie_id = 2
-
-
 # Create a cursor object
 cursor = conn.cursor()
 
@@ -39,33 +35,13 @@ def get_genres(movie_id):
     else:
         return None
 
+    
+def get_movie_details(movie_id):
+    movie_title = get_movie_title(movie_id)
+    genres = get_genres(movie_id)
+    return movie_title, genres
 
-# Get relevance from movie id in table genome scores
-def get_relevance(movie_id):
-    # Execute the SQL query with input variables
-    cursor.execute("SELECT relevance FROM genome_scores WHERE movieId = (?)", (movie_id,))
-
-    # Fetch the row
-    row = cursor.fetchone()
-
-    # Check if a row is found
-    if row:
-        relevance = row[3]
-        return relevance
-    else:
-        return None
-
-# Get tag from movie id in table genome tags
-def get_tag(movie_id):
-    # Execute the SQL query with input variables
-    cursor.execute("SELECT gt.tag FROM genome_tags gt LEFT JOIN genome_scores gs ON gt.tagId = gs.tagId WHERE gs.movieId = (?)", (movie_id,))
-
-    # Fetch the row
-    row = cursor.fetchone()
-
-    # Check if a row is found
-    if row:
-        tag = row[2]
-        return tag
-    else:
-        return None
+# Test run of get_movie_details function
+movie_id = 2
+details = get_movie_details(movie_id)
+print(details)
