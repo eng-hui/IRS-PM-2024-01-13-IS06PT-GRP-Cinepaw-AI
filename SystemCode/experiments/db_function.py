b1ec62d6b2e9ad74403a1eb63bc992a845cfd122
loischee <156448154+loischee@users.noreply.github.com>
2024-03-27 00:54:46 +0800

    Update db_function.py

--- SystemCode/experiments/db_function.py
+++ SystemCode/experiments/db_function.py
@@ -1,45 +1,16 @@
 import sqlite3
+import pandas as pd
+
 
 # Connect to the database
 conn = sqlite3.connect('movies.db')
 
-# Create a cursor object
-cursor = conn.cursor()
-
-#get movie title from movie_id
-def get_movie_title(movie_id):
-    # Execute the SQL query with input variables
-    cursor.execute("SELECT * FROM movies WHERE movieId = (?)", (movie_id,))
-
-    # Fetch the row
-    row = cursor.fetchone()
-
-    # Check if a row is found
-    if row:
-        movie_title = row[2]
-        return movie_title
-    else:
-        return None
-
-# Get genres of a movie based on movie_id
-def get_genres(movie_id):
-    # Execute the SQL query with input variables
-    cursor.execute("SELECT genres FROM movies WHERE movieId = (?)", (movie_id,))
-    
-    # Fetch the row
-    row = cursor.fetchone()
-    # Check if a row is found
-    if row:
-        genres = row[0]
-        return genres
-    else:
-        return None
-
-    
 def get_movie_details(movie_id):
-    movie_title = get_movie_title(movie_id)
-    genres = get_genres(movie_id)
-    return movie_title, genres
+    query = "SELECT * FROM movies WHERE movieId = (?)"
+    movie_df = pd.read_sql(query, conn, params=(movie_id,))
+    movie_name = movie_df['title'].values[0]
+    genres = movie_df['genres'].values[0]    
+    return movie_name,genres
 
 # Test run of get_movie_details function
 movie_id = 2
